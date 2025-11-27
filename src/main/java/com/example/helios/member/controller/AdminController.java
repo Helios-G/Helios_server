f2763adf29ded9c62441802aacd763c1e72eb138
KDI <kedia0528@gmail.com>
2025-11-27 14:36:31 +0900

    initial commit

---
+++ src/main/java/com/example/helios/member/controller/AdminController.java
@@ -0,0 +1,7 @@
+//가입승인, 화이트리스트 관리
+
+package com.example.helios.member.controller;
+
+public class AdminController {
+    
+}
---
+++ src/main/java/com/example/helios/member/controller/MemberController.java
@@ -0,0 +1,7 @@
+//회원가입, 정보 조회/수정/탈퇴
+
+package com.example.helios.member.controller;
+
+class MemberController {
+    
+}
